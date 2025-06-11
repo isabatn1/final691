58b5b8c939197b86b93d72b9c671769f1473ee89
isabatn1 <isabatn1@gmail.com>
2025-06-12 01:52:56 +0300

    Key Vault secret gösteren kod eklendi

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -8,7 +8,12 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
         resp.setContentType("text/plain");
-        resp.getWriter().write("Merhaba Week12 Jakarta!");
+
+        String secret = System.getenv("POSTGRES_CONN");
+
+        resp.getWriter().println("Merhaba Week12 Jakarta!");
+        resp.getWriter().println("Vault Secret: " + secret);
     }
 }
